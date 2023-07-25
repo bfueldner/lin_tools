0ff9347a95c1179853a79f9b48ff2b05a2071215
Benjamin Füldner <benjamin@fueldner.net>
2023-07-25 12:51:32 +0200

    Move signal_encoding to common.

---
+++ test/src/lexical/common/test_signal_encoding.cpp
@@ -0,0 +1,96 @@
+#include <gtest/gtest.h>
+
+#include <lin/lexical/common/signal_encoding.hpp>
+
+TEST(test_lin_lexical_common_signal_encoding, logical_value)
+{
+    namespace x3 = boost::spirit::x3;
+
+    using namespace lin::lexical::common::signal_encoding;
+
+    std::string text{ "logical_value, 0, \"off\";" };
+    logical_value_t logical_value{};
+
+    auto position = text.begin();
+    auto result =
+        phrase_parse(position, text.end(), parser::logical_value, x3::ascii::space, logical_value);
+    ASSERT_TRUE(result);
+    ASSERT_EQ(position, text.end());
+
+    EXPECT_EQ(logical_value.signal_value, 0);
+    EXPECT_STREQ(logical_value.text_info.c_str(), "off");
+}
+
+TEST(test_lin_lexical_common_signal_encoding, physical_value)
+{
+    namespace x3 = boost::spirit::x3;
+
+    using namespace lin::lexical::common::signal_encoding;
+
+    std::string text{ "physical_value, 1, 254, 1, 100, \"lux\";" };
+    physical_range_t physical_range;
+
+    auto position = text.begin();
+    auto result   = phrase_parse(
+        position, text.end(), parser::physical_range, x3::ascii::space, physical_range);
+    ASSERT_TRUE(result);
+    ASSERT_EQ(position, text.end());
+
+    EXPECT_EQ(physical_range.min_value, 1);
+    EXPECT_EQ(physical_range.max_value, 254);
+    EXPECT_EQ(physical_range.scale, 1.0);
+    EXPECT_EQ(physical_range.offset, 100.0);
+    EXPECT_STREQ(physical_range.text_info.c_str(), "lux");
+}
+
+TEST(test_lin_lexical_common_signal_encoding, bcd_value)
+{
+    namespace x3 = boost::spirit::x3;
+
+    using namespace lin::lexical::common::signal_encoding;
+
+    std::string text{ "bcd_value ;" };
+    bcd_value_t bcd_value;
+
+    auto position = text.begin();
+    auto result =
+        phrase_parse(position, text.end(), parser::bcd_value, x3::ascii::space, bcd_value);
+    ASSERT_TRUE(result);
+    ASSERT_EQ(position, text.end());
+}
+
+TEST(test_lin_lexical_common_signal_encoding, encoding_ascii_value)
+{
+    namespace x3 = boost::spirit::x3;
+
+    using namespace lin::lexical::common::signal_encoding;
+
+    std::string text{ "ascii_value ;" };
+    ascii_value_t ascii_value;
+
+    auto position = text.begin();
+    auto result =
+        phrase_parse(position, text.end(), parser::ascii_value, x3::ascii::space, ascii_value);
+    ASSERT_TRUE(result);
+    ASSERT_EQ(position, text.end());
+}
+
+TEST(test_lin_lexical_common_signal_encoding, encoding_value)
+{
+    namespace x3 = boost::spirit::x3;
+
+    using namespace lin::lexical::common::signal_encoding;
+
+    std::string text{ "logical_value, 1, \"on\";" };
+    value_t value;
+
+    auto position = text.begin();
+    auto result   = phrase_parse(position, text.end(), parser::value, x3::ascii::space, value);
+    ASSERT_TRUE(result);
+    ASSERT_EQ(position, text.end());
+
+    auto *logic = std::get_if< logical_value_t >(&value);
+    ASSERT_NE(logic, nullptr);
+    EXPECT_EQ(logic->signal_value, 1);
+    EXPECT_STREQ(logic->text_info.c_str(), "on");
+}
