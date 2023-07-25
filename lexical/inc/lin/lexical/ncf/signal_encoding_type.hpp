341de6cb21adaa6c314302ea75ee43dea1a6456b
Benjamin Füldner <benjamin@fueldner.net>
2023-07-25 12:57:14 +0200

    Replace encoding signals with common::signal_encoding types.

--- lexical/inc/lin/lexical/ncf/signal_encoding_type.hpp
+++ lexical/inc/lin/lexical/ncf/signal_encoding_type.hpp
@@ -6,78 +6,16 @@
 #include <boost/fusion/include/adapt_struct.hpp>
 #include <boost/fusion/include/io.hpp>
 
-#include <lin/lexical/common/bnf.hpp>
+#include <lin/lexical/common/signal_encoding.hpp>
 
 /* 8.2.5.3 Signal encoding type definition */
 
 namespace lin::lexical::ncf {
 
-namespace encoding {
-
-using encoding_name_t = common::bnf::identifier_t;
-
-struct logical_value_t
-{
-    int signal_value{};
-    std::string text_info{};
-};
-
-struct physical_range_t
-{
-    int min_value{};
-    int max_value{};
-    double scale{};
-    double offset{};
-    std::string text_info{};
-};
-
-struct bcd_value_t
-{
-};
-
-struct ascii_value_t
-{
-};
-
-using value_t = std::variant< logical_value_t, physical_range_t, bcd_value_t, ascii_value_t >;
-
-namespace parser {
-
-namespace x3 = boost::spirit::x3;
-
-using namespace common::bnf::parser;
-
-x3::rule< class logical_value, logical_value_t > const logical_value    = "logical_value";
-x3::rule< class physical_range, physical_range_t > const physical_range = "physical_range";
-x3::rule< class bcd_value, bcd_value_t > const bcd_value                = "bcd_value";
-x3::rule< class ascii_value, ascii_value_t > const ascii_value          = "ascii_value";
-x3::rule< class value, value_t > const value                            = "value";
-
-auto const signal_value = integer;
-auto const min_value    = integer;
-auto const max_value    = integer;
-auto const scale        = real_or_integer;
-auto const offset       = real_or_integer;
-auto const text_info    = char_string;
-
-auto const logical_value_def = x3::lit("logical_value") > ',' > signal_value > -(',' > text_info) >
-                               ';';
-auto const physical_range_def = x3::lit("physical_value") > ',' > min_value > ',' > max_value >
-                                ',' > scale > ',' > offset > -(',' > text_info) > ';';
-auto const bcd_value_def   = x3::lit("bcd_value") > ';';
-auto const ascii_value_def = x3::lit("ascii_value") > ';';
-auto const value_def       = logical_value | physical_range | bcd_value | ascii_value;
-
-BOOST_SPIRIT_DEFINE(logical_value, physical_range, bcd_value, ascii_value, value);
-
-}    // namespace parser
-
-}    // namespace encoding
-
 struct encoding_t
 {
-    encoding::encoding_name_t encoding_name{};
-    std::vector< encoding::value_t > value{};
+    common::bnf::identifier_t encoding_name{};
+    std::vector< common::signal_encoding::value_t > value{};
 };
 
 using encodings_t = std::vector< encoding_t >;
@@ -87,7 +25,7 @@ namespace parser {
 namespace x3 = boost::spirit::x3;
 
 using namespace common::bnf::parser;
-using namespace encoding::parser;
+using namespace common::signal_encoding::parser;
 
 /* 8.2.5.3 Signal encoding type definition */
 
@@ -106,12 +44,4 @@ BOOST_SPIRIT_DEFINE(encoding, encoding_definition);
 
 }    // namespace lin::lexical::ncf
 
-BOOST_FUSION_ADAPT_STRUCT(lin::lexical::ncf::encoding::logical_value_t, signal_value, text_info)
-BOOST_FUSION_ADAPT_STRUCT(
-    lin::lexical::ncf::encoding::physical_range_t,
-    min_value,
-    max_value,
-    scale,
-    offset,
-    text_info)
 BOOST_FUSION_ADAPT_STRUCT(lin::lexical::ncf::encoding_t, encoding_name, value)
--- test/src/lexical/ncf/test_signal_encoding_type.cpp
+++ test/src/lexical/ncf/test_signal_encoding_type.cpp
@@ -1,106 +1,13 @@
-#include "lin/lexical/ncf/signal_encoding_type.hpp"
 #include <gtest/gtest.h>
 
-#include <lin/lexical/ncf/free_text.hpp>
-
-TEST(test_lin_lexical_ncf_signal_encoding_type, encoding_logical_value)
-{
-    namespace x3 = boost::spirit::x3;
-
-    using namespace lin::lexical::ncf;
-
-    std::string text{ "logical_value, 0, \"off\";" };
-    encoding::logical_value_t logical_value{};
-
-    auto position = text.begin();
-    auto result =
-        phrase_parse(position, text.end(), parser::logical_value, x3::ascii::space, logical_value);
-    ASSERT_TRUE(result);
-    ASSERT_EQ(position, text.end());
-
-    EXPECT_EQ(logical_value.signal_value, 0);
-    EXPECT_STREQ(logical_value.text_info.c_str(), "off");
-}
-
-TEST(test_lin_lexical_ncf_signal_encoding_type, encoding_physical_value)
-{
-    namespace x3 = boost::spirit::x3;
-
-    using namespace lin::lexical::ncf;
-
-    std::string text{ "physical_value, 1, 254, 1, 100, \"lux\";" };
-    encoding::physical_range_t physical_range;
-
-    auto position = text.begin();
-    auto result   = phrase_parse(
-        position, text.end(), parser::physical_range, x3::ascii::space, physical_range);
-    ASSERT_TRUE(result);
-    ASSERT_EQ(position, text.end());
-
-    EXPECT_EQ(physical_range.min_value, 1);
-    EXPECT_EQ(physical_range.max_value, 254);
-    EXPECT_EQ(physical_range.scale, 1.0);
-    EXPECT_EQ(physical_range.offset, 100.0);
-    EXPECT_STREQ(physical_range.text_info.c_str(), "lux");
-}
-
-TEST(test_lin_lexical_ncf_signal_encoding_type, encoding_bcd_value)
-{
-    namespace x3 = boost::spirit::x3;
-
-    using namespace lin::lexical::ncf;
-
-    std::string text{ "bcd_value ;" };
-    encoding::bcd_value_t bcd_value;
-
-    auto position = text.begin();
-    auto result =
-        phrase_parse(position, text.end(), parser::bcd_value, x3::ascii::space, bcd_value);
-    ASSERT_TRUE(result);
-    ASSERT_EQ(position, text.end());
-}
-
-TEST(test_lin_lexical_ncf_signal_encoding_type, encoding_ascii_value)
-{
-    namespace x3 = boost::spirit::x3;
-
-    using namespace lin::lexical::ncf;
-
-    std::string text{ "ascii_value ;" };
-    encoding::ascii_value_t ascii_value;
-
-    auto position = text.begin();
-    auto result =
-        phrase_parse(position, text.end(), parser::ascii_value, x3::ascii::space, ascii_value);
-    ASSERT_TRUE(result);
-    ASSERT_EQ(position, text.end());
-}
-
-TEST(test_lin_lexical_ncf_signal_encoding_type, encoding_value)
-{
-    namespace x3 = boost::spirit::x3;
-
-    using namespace lin::lexical::ncf;
-
-    std::string text{ "logical_value, 1, \"on\";" };
-    encoding::value_t value;
-
-    auto position = text.begin();
-    auto result   = phrase_parse(position, text.end(), parser::value, x3::ascii::space, value);
-    ASSERT_TRUE(result);
-    ASSERT_EQ(position, text.end());
-
-    auto *logic = std::get_if< encoding::logical_value_t >(&value);
-    ASSERT_NE(logic, nullptr);
-    EXPECT_EQ(logic->signal_value, 1);
-    EXPECT_STREQ(logic->text_info.c_str(), "on");
-}
+#include <lin/lexical/ncf/signal_encoding_type.hpp>
 
 TEST(test_lin_lexical_ncf_signal_encoding_type, encoding)
 {
     namespace x3 = boost::spirit::x3;
 
     using namespace lin::lexical::ncf;
+    using namespace lin::lexical::common;
 
     std::string text{
         "LightEncoding {"
@@ -121,13 +28,13 @@ TEST(test_lin_lexical_ncf_signal_encoding_type, encoding)
 
     // NOLINTBEGIN(readability-container-data-pointer)
     {
-        auto *logic = std::get_if< encoding::logical_value_t >(&encoding.value[0]);
+        auto *logic = std::get_if< signal_encoding::logical_value_t >(&encoding.value[0]);
         ASSERT_NE(logic, nullptr);
         EXPECT_EQ(logic->signal_value, 0);
         EXPECT_STREQ(logic->text_info.c_str(), "Off");
     }
     {
-        auto *physical = std::get_if< encoding::physical_range_t >(&encoding.value[1]);
+        auto *physical = std::get_if< signal_encoding::physical_range_t >(&encoding.value[1]);
         ASSERT_NE(physical, nullptr);
         EXPECT_EQ(physical->min_value, 1);
         EXPECT_EQ(physical->max_value, 254);
@@ -136,7 +43,7 @@ TEST(test_lin_lexical_ncf_signal_encoding_type, encoding)
         EXPECT_STREQ(physical->text_info.c_str(), "lux");
     }
     {
-        auto *logic = std::get_if< encoding::logical_value_t >(&encoding.value[2]);
+        auto *logic = std::get_if< signal_encoding::logical_value_t >(&encoding.value[2]);
         ASSERT_NE(logic, nullptr);
         EXPECT_EQ(logic->signal_value, 255);
         EXPECT_STREQ(logic->text_info.c_str(), "error");
@@ -149,6 +56,7 @@ TEST(test_lin_lexical_ncf_signal_encoding_type, encodings)
     namespace x3 = boost::spirit::x3;
 
     using namespace lin::lexical::ncf;
+    using namespace lin::lexical::common;
 
     std::string text{
         "encoding {"
@@ -177,25 +85,25 @@ TEST(test_lin_lexical_ncf_signal_encoding_type, encodings)
     ASSERT_EQ(encodings[0].value.size(), 4);
     // NOLINTBEGIN(readability-container-data-pointer)
     {
-        auto *logic = std::get_if< encoding::logical_value_t >(&encodings[0].value[0]);
+        auto *logic = std::get_if< signal_encoding::logical_value_t >(&encodings[0].value[0]);
         ASSERT_NE(logic, nullptr);
         EXPECT_EQ(logic->signal_value, 0);
         EXPECT_STREQ(logic->text_info.c_str(), "off");
     }
     {
-        auto *logic = std::get_if< encoding::logical_value_t >(&encodings[0].value[1]);
+        auto *logic = std::get_if< signal_encoding::logical_value_t >(&encodings[0].value[1]);
         ASSERT_NE(logic, nullptr);
         EXPECT_EQ(logic->signal_value, 1);
         EXPECT_STREQ(logic->text_info.c_str(), "on");
     }
     {
-        auto *logic = std::get_if< encoding::logical_value_t >(&encodings[0].value[2]);
+        auto *logic = std::get_if< signal_encoding::logical_value_t >(&encodings[0].value[2]);
         ASSERT_NE(logic, nullptr);
         EXPECT_EQ(logic->signal_value, 2);
         EXPECT_STREQ(logic->text_info.c_str(), "error");
     }
     {
-        auto *logic = std::get_if< encoding::logical_value_t >(&encodings[0].value[3]);
+        auto *logic = std::get_if< signal_encoding::logical_value_t >(&encodings[0].value[3]);
         ASSERT_NE(logic, nullptr);
         EXPECT_EQ(logic->signal_value, 3);
         EXPECT_STREQ(logic->text_info.c_str(), "void");
@@ -205,13 +113,13 @@ TEST(test_lin_lexical_ncf_signal_encoding_type, encodings)
     EXPECT_EQ(encodings[1].value.size(), 2);
     // NOLINTBEGIN(readability-container-data-pointer)
     {
-        auto *logic = std::get_if< encoding::logical_value_t >(&encodings[1].value[0]);
+        auto *logic = std::get_if< signal_encoding::logical_value_t >(&encodings[1].value[0]);
         ASSERT_NE(logic, nullptr);
         EXPECT_EQ(logic->signal_value, 0);
         EXPECT_STREQ(logic->text_info.c_str(), "OK");
     }
     {
-        auto *logic = std::get_if< encoding::logical_value_t >(&encodings[1].value[1]);
+        auto *logic = std::get_if< signal_encoding::logical_value_t >(&encodings[1].value[1]);
         ASSERT_NE(logic, nullptr);
         EXPECT_EQ(logic->signal_value, 1);
         EXPECT_STREQ(logic->text_info.c_str(), "error");
