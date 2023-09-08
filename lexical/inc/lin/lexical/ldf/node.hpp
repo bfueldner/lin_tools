7bcf171540e11ad09683d10435f1a8a5d55a3155
Benjamin Füldner <benjamin@fueldner.net>
2023-09-08 11:21:22 +0200

    Implement 9.2.2.2 node attributes.

--- lexical/inc/lin/lexical/ldf/node.hpp
+++ lexical/inc/lin/lexical/ldf/node.hpp
@@ -67,6 +67,170 @@ BOOST_SPIRIT_DEFINE(participating_nodes);
 
 }    // namespace parser
 
+/* 9.2.2.2 Node attributes */
+
+namespace node_attribute {
+
+using protocol_version_t = common::bnf::char_string_t;
+using diag_address_t     = common::bnf::integer_t;
+
+using lin_protocol_t   = protocol_version_t;
+using configured_nad_t = diag_address_t;
+using initial_nad_t    = diag_address_t;
+
+using supplier_id_t = common::bnf::integer_t;
+using function_id_t = common::bnf::integer_t;
+using variant_t     = common::bnf::integer_t;
+using signal_name_t = common::bnf::identifier_t;
+
+struct product_id_t
+{
+    supplier_id_t supplier_id{};
+    function_id_t function_id{};
+    std::optional< variant_t > variant{};
+};
+
+using response_error_t      = signal_name_t;
+using fault_state_signals_t = std::vector< signal_name_t >;
+using p2_min_t              = common::bnf::real_or_integer_t;
+using st_min_t              = common::bnf::real_or_integer_t;
+using n_as_timeout_t        = common::bnf::real_or_integer_t;
+using n_cr_timeout_t        = common::bnf::real_or_integer_t;
+
+using frame_name_t = common::bnf::identifier_t;
+using message_id_t = common::bnf::integer_t;
+
+struct configurable_frame_t
+{
+    frame_name_t frame_name{};
+    std::optional< message_id_t > message_id{};
+};
+using configurable_frames_t = std::vector< configurable_frame_t >;
+
+namespace parser {
+
+namespace x3 = boost::spirit::x3;
+
+using namespace common::bnf::parser;
+
+x3::rule< class lin_protocol, lin_protocol_t > const lin_protocol       = "lin_protocol";
+x3::rule< class configured_nad, configured_nad_t > const configured_nad = "configured_nad";
+x3::rule< class initial_nad, initial_nad_t > const initial_nad          = "initial_nad";
+
+x3::rule< class product_id, product_id_t > const product_id             = "product_id";
+x3::rule< class response_error, response_error_t > const response_error = "response_error";
+x3::rule< class fault_state_signals, fault_state_signals_t > const fault_state_signals =
+    "fault_state_signals";
+x3::rule< class p2_min, p2_min_t > const p2_min                   = "p2_min";
+x3::rule< class st_min, st_min_t > const st_min                   = "st_min";
+x3::rule< class n_as_timeout, n_as_timeout_t > const n_as_timeout = "n_as_timeout";
+x3::rule< class n_cr_timeout, n_cr_timeout_t > const n_cr_timeout = "n_cr_timeout";
+x3::rule< class configurable_frame, configurable_frame_t > const configurable_frame =
+    "configurable_frame";
+x3::rule< class configurable_frames, configurable_frames_t > const configurable_frames =
+    "configurable_frames";
+
+auto const protocol_version = char_string;
+auto const diag_address     = integer;
+
+auto const lin_protocol_def   = x3::lit("LIN_protocol") > '=' > protocol_version > ';';
+auto const configured_nad_def = x3::lit("configured_NAD") > '=' > diag_address > ';';
+auto const initial_nad_def    = x3::lit("initial_NAD") > '=' > diag_address > ';';
+
+auto const supplier_id = integer;
+auto const function_id = integer;
+auto const variant     = integer;
+auto const signal_name = identifier;
+auto const frame_name  = identifier;
+auto const message_id  = integer;
+
+auto const product_id_def = x3::lit("product_id") > '=' > supplier_id > ',' > function_id >
+                            -(',' > variant) > ';';
+auto const response_error_def      = x3::lit("response_error") > '=' > signal_name > ';';
+auto const fault_state_signals_def = x3::lit("fault_state_signals") > '=' > signal_name % ',' > ';';
+auto const p2_min_def              = x3::lit("P2_min") > '=' > real_or_integer > "ms" > ';';
+auto const st_min_def              = x3::lit("ST_min") > '=' > real_or_integer > "ms" > ';';
+auto const n_as_timeout_def        = x3::lit("N_As_timeout") > '=' > real_or_integer > "ms" > ';';
+auto const n_cr_timeout_def        = x3::lit("N_Cr_timeout") > '=' > real_or_integer > "ms" > ';';
+auto const configurable_frame_def  = frame_name > -('=' > message_id) > ';';
+auto const configurable_frames_def = x3::lit("configurable_frames") > '{' > *configurable_frame >
+                                     '}';
+
+BOOST_SPIRIT_DEFINE(
+    lin_protocol,
+    configured_nad,
+    initial_nad,
+    product_id,
+    response_error,
+    fault_state_signals,
+    p2_min,
+    st_min,
+    n_as_timeout,
+    n_cr_timeout,
+    configurable_frame,
+    configurable_frames);
+
+}    // namespace parser
+
+}    // namespace node_attribute
+
+struct node_attribute_t
+{
+    common::bnf::identifier_t node_name{};
+    node_attribute::lin_protocol_t lin_protocol{};
+    node_attribute::configured_nad_t configured_nad{};
+    std::optional< node_attribute::initial_nad_t > initial_nad{};
+    std::optional< node_attribute::product_id_t > product_id{};
+    node_attribute::response_error_t response_error{};
+    node_attribute::fault_state_signals_t fault_state_signals{};
+    node_attribute::p2_min_t p2_min{ 50.0 };
+    node_attribute::st_min_t st_min{ 0.0 };
+    node_attribute::n_as_timeout_t n_as_timeout{ 1000.0 };
+    node_attribute::n_cr_timeout_t n_cr_timeout{ 1000.0 };
+    node_attribute::configurable_frames_t configurable_frames{};
+};
+using node_attributes_t = std::vector< node_attribute_t >;
+
+namespace parser {
+
+namespace x3 = boost::spirit::x3;
+
+//using namespace common::bnf::parser;
+using namespace node_attribute::parser;
+
+x3::rule< class node_attribute, node_attribute_t > const node_attribute    = "node_attribute";
+x3::rule< class node_attributes, node_attributes_t > const node_attributes = "node_attributes";
+
+auto node_name_action           = [](auto &ctx) { x3::_val(ctx).node_name = x3::_attr(ctx); };
+auto lin_protocol_action        = [](auto &ctx) { x3::_val(ctx).lin_protocol = x3::_attr(ctx); };
+auto configured_nad_action      = [](auto &ctx) { x3::_val(ctx).configured_nad = x3::_attr(ctx); };
+auto initial_nad_action         = [](auto &ctx) { x3::_val(ctx).initial_nad = x3::_attr(ctx); };
+auto product_id_action          = [](auto &ctx) { x3::_val(ctx).product_id = x3::_attr(ctx); };
+auto response_error_action      = [](auto &ctx) { x3::_val(ctx).response_error = x3::_attr(ctx); };
+auto fault_state_signals_action = [](auto &ctx) {
+    x3::_val(ctx).fault_state_signals = x3::_attr(ctx);
+};
+auto p2_min_action              = [](auto &ctx) { x3::_val(ctx).p2_min = x3::_attr(ctx); };
+auto st_min_action              = [](auto &ctx) { x3::_val(ctx).st_min = x3::_attr(ctx); };
+auto n_as_timeout_action        = [](auto &ctx) { x3::_val(ctx).n_as_timeout = x3::_attr(ctx); };
+auto n_cr_timeout_action        = [](auto &ctx) { x3::_val(ctx).n_cr_timeout = x3::_attr(ctx); };
+auto configurable_frames_action = [](auto &ctx) {
+    x3::_val(ctx).configurable_frames = x3::_attr(ctx);
+};
+
+auto const node_attribute_def =
+    node_name[node_name_action] > '{' >
+    *(lin_protocol[lin_protocol_action] | configured_nad[configured_nad_action] |
+      initial_nad[initial_nad_action] | product_id[product_id_action] |
+      response_error[response_error_action] | fault_state_signals[fault_state_signals_action] |
+      p2_min[p2_min_action] | st_min[st_min_action] | n_as_timeout[n_as_timeout_action] |
+      n_cr_timeout[n_cr_timeout_action] | configurable_frames[configurable_frames_action]) > '}';
+auto const node_attributes_def = x3::lit("Node_attributes") > '{' > *node_attribute > '}';
+
+BOOST_SPIRIT_DEFINE(node_attribute, node_attributes);
+
+}    // namespace parser
+
 }    // namespace lin::lexical::ldf::node
 
 BOOST_FUSION_ADAPT_STRUCT(
@@ -75,3 +239,28 @@ BOOST_FUSION_ADAPT_STRUCT(
     time_base,
     jitter)
 BOOST_FUSION_ADAPT_STRUCT(lin::lexical::ldf::node::participating_nodes_t, master, slaves)
+
+BOOST_FUSION_ADAPT_STRUCT(
+    lin::lexical::ldf::node::node_attribute::product_id_t,
+    supplier_id,
+    function_id,
+    variant)
+BOOST_FUSION_ADAPT_STRUCT(
+    lin::lexical::ldf::node::node_attribute::configurable_frame_t,
+    frame_name,
+    message_id)
+
+BOOST_FUSION_ADAPT_STRUCT(
+    lin::lexical::ldf::node::node_attribute_t,
+    node_name,
+    lin_protocol,
+    configured_nad,
+    initial_nad,
+    product_id,
+    response_error,
+    fault_state_signals,
+    p2_min,
+    st_min,
+    n_as_timeout,
+    n_cr_timeout,
+    configurable_frames)
